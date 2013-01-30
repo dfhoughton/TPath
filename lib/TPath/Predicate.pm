d2c5b2ce1733ccbe75100e1af2d1abbe160efab1
David Houghton <dhoughton@complyoninc.com>
2013-01-30 11:33:47 -0500

    created Predicate role

---
+++ lib/TPath/Predicate.pm
@@ -0,0 +1,16 @@
+package TPath::Predicate;
+
+# ABSTRACT : interface of square bracket sub-expressions in TPath expressions
+
+use Moose::Role;
+
+=method filter
+
+Takes a collection of nodes and an index and returns the collection of nodes
+for which the predicate is true.
+
+=cut
+
+requires 'filter';
+
+1;
