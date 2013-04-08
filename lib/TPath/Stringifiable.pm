02b98af093126fa28a7bab4964799c45bdf778d8
dfhoughton <dfhoughton@gmail.com>
2013-04-08 10:08:26 -0400

    made expressions stringifiable

---
+++ lib/TPath/Stringifiable.pm
@@ -0,0 +1,64 @@
+package TPath::Stringifiable;
+
+# ABSTRACT: role requiring that a class have a to_string method
+
+=head1 DESCRIPTION
+
+Role that enforces the presence of a to_string method. Makes sure the absence of this method
+where it is expected will be a compile time rather than run time error.
+
+=cut
+
+use Moose::Role;
+use Scalar::Util qw(looks_like_number);
+
+=head1 REQUIRED ROLES
+
+=head2 to_string
+
+Produces a sensible, human-readable stringification of the object. Some implementations of the method
+may expect parameters.
+
+=cut
+
+requires 'to_string';
+
+# method available to to_string that adds escape characters as needed
+# params: string -- string to escape
+#         chars  -- characters to escape -- \ always added
+sub _escape {
+	my ( $self, $string, @chars ) = @_;
+	my $s = '';
+	my %chars = map { $_ => 1 } @chars, '\\';
+	for my $c ( split //, $string ) {
+		$s .= '\\' if $chars{$c};
+		$s .= $c;
+	}
+	return $s;
+}
+
+# general stringification procedure
+sub _stringify {
+	my ( $self, $arg ) = @_;
+	return $arg->to_string if blessed $arg && $arg->can('to_string');
+	confess 'unexpected argument type: ' . ref $arg if ref $arg;
+	return $arg if looks_like_number $arg;
+	return "'" . $self->_escape( $arg, "'" ) . "'";
+}
+
+# converts some label -- tag name or attribute name -- into a parsable string
+sub _stringify_label {
+	my ( $self, $string, $first ) = @_;
+	return $string
+	  if $string =~
+	  /^(?>\\.|[\p{L}\$_])(?>[\p{L}\$\p{N}_]|[-.:](?=[\p{L}_\$\p{N}])|\\.)*+$/;
+	return ( $first ? ':' : '' ) . '"' . $self->_escape($string) . '"'
+	  unless ( index $string, '"' ) > -1;
+	return ( $first ? ':' : '' ) . "'" . $self->_escape($string) . "'"
+	  unless ( index $string, "'" ) > -1;
+
+	# safety fallback
+	return $self->_escape( $string, grep { !/[\p{L}]\$_/ } split //, $string );
+}
+
+1;
---
+++ t/stringification.t
@@ -0,0 +1,65 @@
+# runs through some basic expressions
+
+use strict;
+use warnings;
+use File::Basename qw(dirname);
+
+BEGIN {
+	push @INC, dirname($0);
+}
+
+use Test::More;
+use Test::Exception;
+use ToyXMLForester;
+
+my $f = ToyXMLForester->new;
+
+my @paths = grep /^\s*+\S(?<!#)/, <<'EOF' =~ /.*/mg;
+a
+//a
+//*[@attr('b')]
+descendant::*
+descendant::@attr('b')
+^a
+/>a
+/>@attr('b')
+a[@attr('b') or @attr('c')]
+a[@attr('b') and @attr('c')]
+a[@attr('b') one @attr('c')]
+a[!@attr('b') one @attr('c')]
+a[@attr('b') or @attr('c') and @attr('d')]
+/a/b
+/a//b
+:"a b"
+/a/"b c"
+/a//"b c"
+a[1]
+a[b]
+a[@attr('b')]
+a[@attr('b') = 1]
+a[@attr('b') == 1]
+a[@attr('b') =~ 'b']
+^a
+^@attr('b')
+~a~
+~a~~b~
+a?
+a+
+a*
+a{,2}
+a{2,}
+a{2}
+(a/b){2}
+:root/a
+:id(foo)/a
+EOF
+
+plan tests => @paths * 3;
+
+for my $path (@paths) {
+	my ($p1, $p2);
+	lives_ok { $p1 = $f->path($path) };
+	lives_ok { $p2 = $f->path("$p1") };
+	is_deeply $p1, $p2;
+}
+done_testing();
